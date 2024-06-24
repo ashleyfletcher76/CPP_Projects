608bd7b0aa6df570a83e179f5d6b5bd4920884b1
Ashley Fletcher <asfletch@1-E-31.42heilbronn.de>
2024-06-24 16:13:52 +0200

    Finished cpp06

--- ex00/src/ConversionFunctions.cpp
+++ ex00/src/ConversionFunctions.cpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/23 11:36:20 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/24 14:52:03 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/24 16:10:41 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,69 +29,48 @@ void	convertChar(std::string& toConvert)
 
 void	convertInt(std::string& toConvert)
 {
-	try
-	{
-		int	i = atof(toConvert.c_str());
-		if (i < std::numeric_limits<char>::min()
-			|| i > std::numeric_limits<char>::max() || !std::isprint(i))
-			std::cout << "char: Non displayable." << std::endl;
-		else
-			std::cout << "char: '" << static_cast<char>(i) << "'" << std::endl;
-		std::cout << "int: " << i << std::endl;
-		float	f = static_cast<float>(i);
-		std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
-		double	d = static_cast<double>(i);
-		std::cout << "double: " << d << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "Conversion to int failed: " << e.what() << '\n';
-	}
+	int	i = atof(toConvert.c_str());
+	if (i < std::numeric_limits<char>::min()
+		|| i > std::numeric_limits<char>::max() || !std::isprint(i))
+		std::cout << "char: Non displayable." << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(i) << "'" << std::endl;
+	std::cout << "int: " << i << std::endl;
+	float	f = static_cast<float>(i);
+	std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
+	double	d = static_cast<double>(i);
+	std::cout << "double: " << d << std::endl;
 }
 
 void	convertDouble(std::string& toConvert)
 {
-	try
-	{
-		double	d = atof(toConvert.c_str());
-		if (checkMaxMinInt(d))
-			return ;
-		if (d < std::numeric_limits<char>::min() || d > std::numeric_limits<char>::max()
-			|| !std::isprint(static_cast<int>(d)))
-			std::cout << "char: Non displayable." << std::endl;
-		else
-			std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
-		int	i = static_cast<int>(d);
-		std::cout << "int: " << i << std::endl;
-		float	f = static_cast<float>(d);
-		std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
-		std::cout << "double: " << d << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "Conversion to double failed: " << e.what() << '\n';
-	}
+	double	d = atof(toConvert.c_str());
+	if (checkMaxMinInt(d))
+		return ;
+	if (d < std::numeric_limits<char>::min() || d > std::numeric_limits<char>::max()
+		|| !std::isprint(static_cast<int>(d)))
+		std::cout << "char: Non displayable." << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(d) << "'" << std::endl;
+	int	i = static_cast<int>(d);
+	std::cout << "int: " << i << std::endl;
+	float	f = static_cast<float>(d);
+	std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
+	std::cout << "double: " << d << std::endl;
 }
 
 void	convertFloat(std::string& toConvert)
 {
-	try
-	{
-		float f = atof(toConvert.c_str());
-		if (f < std::numeric_limits<char>::min() || f > std::numeric_limits<char>::max()
-			|| !std::isprint(static_cast<int>(f)))
-			std::cout << "char: Non displayable." << std::endl;
-		else
-			std::cout << "char: '" << static_cast<char>(f) << "'" << std::endl;
-		int	i = static_cast<int>(f);
-		std::cout << "int: " << i << std::endl;
-		std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
-		double	d = static_cast<double>(f);
-		std::cout << "double: " << d << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << "Conversion to float failed: " << e.what() << '\n';
-	}
+	float f = atof(toConvert.c_str());
+	if (f < std::numeric_limits<char>::min() || f > std::numeric_limits<char>::max()
+		|| !std::isprint(static_cast<int>(f)))
+		std::cout << "char: Non displayable." << std::endl;
+	else
+		std::cout << "char: '" << static_cast<char>(f) << "'" << std::endl;
+	int	i = static_cast<int>(f);
+	std::cout << "int: " << i << std::endl;
+	std::cout << std::fixed << std::setprecision(1) << "float: " << f << "f" << std::endl;
+	double	d = static_cast<double>(f);
+	std::cout << "double: " << d << std::endl;
 }
 
