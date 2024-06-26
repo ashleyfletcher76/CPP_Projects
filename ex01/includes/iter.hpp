528fa701905d2220fc477bf0cb58cb6b7b516f75
Ashley Fletcher <asfletch@2-A-4.42heilbronn.de>
2024-06-26 12:21:33 +0200

    ex01 finished. Not much to it, just understanding how functions interact with an array using templates. Tests implemented

--- ex01/includes/iter.hpp
+++ ex01/includes/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:01:40 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/25 16:02:05 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/26 12:15:26 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,32 @@
 
 #include <iostream>
 
+template<typename A, typename F>
+void	iter(A* array, std::size_t len, F function)
+{
+	for(std::size_t i = 0; i < len; i++)
+		function(array[i]);
+}
 
+template<typename T>
+void	square(T num)
+{
+	T result;
+
+	result = num * num;
+	std::cout << num << " * " << num << " = " << result << std::endl;
+}
+
+template<typename T>
+void	printInt(T num)
+{
+	std::cout << num << " ";
+}
+
+template<typename T>
+void	printString(T str)
+{
+	std::cout << "'" << str << "' ";
+}
 
 #endif
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,10 +6,46 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:01:06 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/25 16:01:27 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/26 12:20:45 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
 
+void	charsString()
+{
+	std::string	testString1[] = {"Hello", "I am", "A chicken"};
+	char		testString2[] = "Hello";
+	std::size_t	num1Len = sizeof(testString1) / sizeof(testString1[0]);
+	std::size_t	num2Len = sizeof(testString2) - 1;
 
+	std::cout << "Test testString1 squared" << std::endl;
+	std::cout << std::endl;
+	iter(testString1, num1Len, printString<std::string>);
+	std::cout << std::endl;
+	std::cout << "------" << std::endl;
+	std::cout << "Test testString2 print" << std::endl;
+	iter(testString2, num2Len, printString<char>);
+	std::cout << std::endl;
+}
+
+void	nums()
+{
+	int	num1[] = {5,6};
+	int	num2[] = {5, 6, 7, 8};
+	std::size_t	num1Len = sizeof(num1) / sizeof(num1[0]);
+	std::size_t	num2Len = sizeof(num2) / sizeof(num2[0] - 1);
+
+	std::cout << "Test num1 squared" << std::endl;
+	iter(num1, num1Len, square<int>);
+	std::cout << "------" << std::endl;
+	std::cout << "Test num2 print" << std::endl;
+	iter(num2, num2Len, printInt<int>);
+	std::cout << std::endl;
+}
+
+int	main()
+{
+	//nums();
+	charsString();
+}
