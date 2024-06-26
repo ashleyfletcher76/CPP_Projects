6449a5305e18cb6898e168122bf71752e83f6485
Ashley Fletcher <asfletch@2-A-4.42heilbronn.de>
2024-06-26 12:59:45 +0200

    Stages for ex02 being implemented. Learning about class templates

--- ex02/includes/Array.hpp
+++ ex02/includes/Array.hpp
@@ -6,13 +6,42 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 12:22:50 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/26 12:23:08 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/26 12:59:14 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+#include <iostream>
+#include <exception>
+#include <cstdlib>
+
+template<typename T>
+class Array
+{
+	private:
+
+
+	public:
+		Array();
+		Array(unsigned int n);
+		~Array();
+		Array(const Array& other);
+		Array& operator=(const Array& other);
+		T& operator[](int i);
+		size_t	size() const;
+};
+
+Array::Array() {}
+
+Array::Array(unsigned int n) {}
+
+Array::~Array()	{}
+
+Array::Array(const Array& other) {}
+
+Array& Array::operator=(const Array& other) {return (*this);}
 
 
 #endif
