9674c1dc60d09c26182552c60c1e5d16ffeb7125
Ashley Fletcher <asfletch@1-E-32.42heilbronn.de>
2024-06-26 17:43:30 +0200

    Finished version

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 15:44:19 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/25 15:51:36 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/26 17:32:02 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,12 @@ int	main()
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	char c_char = 'a';
+	char d_char = 'b';
+	std::cout << "Before char swap" << std::endl;
+	std::cout << "c = " << c_char << ", d = " << d_char << std::endl;
+	::swap(c_char, d_char);
+	std::cout << "After char swap" << std::endl;
+	std::cout << "c = " << c_char << ", d = " << d_char << std::endl;
 	return 0;
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 16:01:06 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/26 12:20:45 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/26 17:37:28 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ void	charsString()
 	std::size_t	num1Len = sizeof(testString1) / sizeof(testString1[0]);
 	std::size_t	num2Len = sizeof(testString2) - 1;
 
-	std::cout << "Test testString1 squared" << std::endl;
+	std::cout << "Test testString1 strings" << std::endl;
 	std::cout << std::endl;
 	iter(testString1, num1Len, printString<std::string>);
 	std::cout << std::endl;
 	std::cout << "------" << std::endl;
-	std::cout << "Test testString2 print" << std::endl;
+	std::cout << "Test testString2 chars" << std::endl;
 	iter(testString2, num2Len, printString<char>);
 	std::cout << std::endl;
 }
@@ -34,7 +34,7 @@ void	nums()
 	int	num1[] = {5,6};
 	int	num2[] = {5, 6, 7, 8};
 	std::size_t	num1Len = sizeof(num1) / sizeof(num1[0]);
-	std::size_t	num2Len = sizeof(num2) / sizeof(num2[0] - 1);
+	std::size_t	num2Len = sizeof(num2) / sizeof(num2[0]);
 
 	std::cout << "Test num1 squared" << std::endl;
 	iter(num1, num1Len, square<int>);
@@ -46,6 +46,6 @@ void	nums()
 
 int	main()
 {
-	//nums();
+	nums();
 	charsString();
 }
