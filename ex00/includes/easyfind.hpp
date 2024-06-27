b080dde475eb2be9cbd656a03a5d85c90643acae
Ashley Fletcher <asfletch@1-A-32.42heilbronn.de>
2024-06-27 19:04:19 +0200

    First exercise done. Need to add extra tests

--- ex00/includes/easyfind.hpp
+++ ex00/includes/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 11:17:25 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/27 11:17:59 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/27 17:05:58 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,27 @@
 # define EASYFIND_HPP
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <exception>
+
+class NotFoundException : public std::exception
+{
+	public:
+		const char* what() const throw() {
+			return ("Not found exception!");
+		}
+};
+
+template<typename T>
+typename T::iterator easyfind(T& container, int num)
+{
+	typename T::iterator it = std::find(container.begin(), container.end(), num);
+	if (it == container.end())
+		throw NotFoundException();
+	return (it);
+}
 
 
 #endif
--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,14 +6,28 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 11:17:08 by asfletch          #+#    #+#             */
-/*   Updated: 2024/06/27 11:19:10 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/06/27 17:10:13 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "easyfind.hpp"
-#include "easyfind.tpp"
 
 int	main()
 {
-	
+	std::vector<int> intputVector;
+	intputVector.push_back(1);
+	intputVector.push_back(2);
+	intputVector.push_back(3);
+	intputVector.push_back(4);
+	std::cout << "==== TEST 1 ====" << std::endl;
+	try
+	{
+		std::vector<int>::iterator it = easyfind(intputVector, 3);
+		std::cout << "Number found: " << *it << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+
 }
