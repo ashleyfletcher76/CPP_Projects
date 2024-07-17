8e3eadc5829e6b5152f027af9c7bc3d0b58e2624
Ashley Fletcher <asfletch@3-C-6.42heilbronn.de>
2024-07-17 18:36:16 +0200

    Most functionality working. I have the exchange rate, now I need to check edge cases

--- ex00/includes/BitcoinExchange.hpp
+++ ex00/includes/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 13:08:09 by asfletch          #+#    #+#             */
-/*   Updated: 2024/07/17 17:21:32 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/07/17 18:15:05 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,15 +25,19 @@ class BitcoinExchange
 	private:
 		std::map<std::string, double> _btcPrices;
 		std::string _argv;
+		std::string	_date;
+		float		_amount;
 		int		readDatabase();
 		int		readInput();
+		void	takeAmount(std::string& line);
+		void	calculateExchange(const std::string& date, float rate);
 
 	public:
 		BitcoinExchange();
 		~BitcoinExchange();
 		BitcoinExchange(const BitcoinExchange& other);
 		BitcoinExchange& operator=(const BitcoinExchange& other);
-		
+
 		void	beginProcess(const std::string& file);
 };
 
--- ex00/src/BitcoinExchange.cpp
+++ ex00/src/BitcoinExchange.cpp
@@ -6,22 +6,27 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 13:11:17 by asfletch          #+#    #+#             */
-/*   Updated: 2024/07/17 17:30:14 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/07/17 18:35:13 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 
-BitcoinExchange::BitcoinExchange() : _argv(NULL) {}
+BitcoinExchange::BitcoinExchange() {}
 
 BitcoinExchange::~BitcoinExchange() {}
 
-BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _btcPrices(other._btcPrices) {}
+BitcoinExchange::BitcoinExchange(const BitcoinExchange& other) : _btcPrices(other._btcPrices), _argv(other._argv),
+_amount(other._amount) {}
 
 BitcoinExchange& BitcoinExchange::operator=(const BitcoinExchange& other)
 {
 	if (this != &other)
+	{
 		_btcPrices = other._btcPrices;
+		_argv = other._argv;
+		_amount = other._amount;
+	}
 	return(*this);
 }
 
@@ -63,7 +68,7 @@ int	BitcoinExchange::readDatabase()
 int	BitcoinExchange::readInput()
 {
 	std::string line;
-	std::ifstream input(_argv);
+	std::ifstream input(_argv.c_str());
 	if (!input)
 	{
 		std::cerr << "Could not open input file." << std::endl;
@@ -71,20 +76,43 @@ int	BitcoinExchange::readInput()
 	}
 	while(std::getline(input, line))
 	{
-		std::map<std::string, double>::iterator iter = _btcPrices.find(line);
+		takeAmount(line);
+		std::map<std::string, double>::iterator iter = _btcPrices.find(_date);
 		if (iter != _btcPrices.end())
-			std::cout << line << " = " << iter->second << std::endl;
+		{
+			calculateExchange(_date, iter->second);
+		}
 		else
 		{
-			iter = _btcPrices.lower_bound(line);
+			iter = _btcPrices.lower_bound(_date);
 			if (iter == _btcPrices.begin())
 				std::cout << "No date available." << std::endl;
 			else
 			{
 				--iter;
-				std::cout << "Closest price on " << iter->first << " is " << iter->second << std::endl;
+				calculateExchange(iter->first, iter->second);
 			}
 		}
 	}
 	return (0);
+}
+
+void	BitcoinExchange::takeAmount(std::string& line)
+{
+	std::size_t pos = line.find('|');
+	if (pos != std::string::npos)
+	{
+		_date = line.substr(0, pos - 1);
+		_date.erase(_date.find_last_not_of("\n\r\t") + 1);
+		//std::cout << "Date: " << _date << "." << std::endl;
+		std::string value = line.substr(pos + 1);
+		_amount = std::strtof(value.c_str(), NULL);
+		//std::cout << "Amount: " << _amount << "." << std::endl;
+	}
+}
+
+void	BitcoinExchange::calculateExchange(const std::string& date, float rate)
+{
+	float	exchange = _amount * rate;
+	std::cout << date << " => " << _amount << " = " << exchange << std::endl;
 }
