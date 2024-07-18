cb8d31e1b0ca3c49e9786cc4b14db143adfba51b
Ashley Fletcher <asfletch@3-C-6.42heilbronn.de>
2024-07-18 17:01:09 +0200

    Added another main so someone can also test directly from the command line

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,13 +6,14 @@
 /*   By: asfletch <asfletch@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 12:53:18 by asfletch          #+#    #+#             */
-/*   Updated: 2024/07/18 16:55:21 by asfletch         ###   ########.fr       */
+/*   Updated: 2024/07/18 17:00:43 by asfletch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RPN.hpp"
 
 // 0 - 2 work
+// 3 and 4 are the same as 1 and 3 but no space
 // 3 - 5 dont work
 
 std::string argvCalculate(int arg)
@@ -30,12 +31,18 @@ std::string argvCalculate(int arg)
 			str = "9 6 3 / * 2 +";
 			break ;
 		case 3:
-			str = "5 +";
+			str = "512+4*+3-";
 			break ;
 		case 4:
-			str = "5 3 + a";
+			str = "963/*2+";
 			break ;
 		case 5:
+			str = "5 +";
+			break ;
+		case 6:
+			str = "5 3 + a";
+			break ;
+		case 7:
 			str = "2 3 + 4";
 			break ;
 		default:
@@ -72,3 +79,24 @@ int	main(int argc, char **argv)
 	}
 	return (0);
 }
+
+// int	main(int argc, char **argv)
+// {
+// 	if (argc != 2)
+// 	{
+// 		std::cerr << "Incorrect args" << std::endl;
+// 		return (1);
+// 	}
+// 	try
+// 	{
+// 		RPN calculator;
+// 		int	result = calculator.calculate(argv[1]);
+// 		std::cout << result << std::endl;
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		std::cerr << e.what() << '\n';
+// 		return (1);
+// 	}
+// 	return (0);
+// }
